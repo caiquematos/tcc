d90a4b30d6985beb249a47d66a4991e7a3294b6f
Caique Neiva <caique@sysvale.com>
2015-08-18 15:21:25 -0300

    trying to repeat cards

--- web/Sup/app/views/coordenadores.blade.php
+++ web/Sup/app/views/coordenadores.blade.php
@@ -1,23 +1,26 @@
+
 <div class="row">
         <div class="col s12 m3">
+          @foreach($coordinators   as $coordinator)
           <div class="card hoverable">
             <div class="card-image">
               <img src="img/xbee.png" height="200" width="100">
               <span class="card-title"></span>
             </div>
             <div class="card-content center">
-                <h5>Coordenador 01</h5>
-                Número de Módulos: 02
-                <br/><i class="zmdi zmdi-battery"></i> 33%
+                <h5>Coordenador {{$coordinator->id}}</h5>
+                Número de Módulos: 
+                <br/><i class="zmdi zmdi-battery"></i> {{$coordinator->battery}}
             </div>
             <div class="card-action center">
               <div class="switch">
                 <label>Off
-                <input type="checkbox">
+                <input type="checkbox" checked={{$coordinator->status}}>
                 <span class="lever"></span>On
                 </label>
                 </div>
             </div>
           </div>
+          @endforeach
         </div>          
 </div>
